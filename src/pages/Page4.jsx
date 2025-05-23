4b6ad58f8bdd83281db2422ee405b5afa521c440
Zeeshan <zh012948@gmail.com>
2025-05-23 20:55:55 +0500

    All is Well

--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -1,9 +1,26 @@
-
+import Image from '../assets/images/pana.svg'
 import '../styles/Page4.css'
 function Page4() {
     return (
         <>
-            This is Page 4
+            <div className="page4-container">
+
+                <div className="image-container">
+                    <img src={Image} alt="Image" />
+                </div>
+
+                <div className="content-container">
+                    <p className="heading">How to design your site footer<br />like we did</p>
+
+                    <div className="content-para">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, error dolorem. Quis voluptate adipisci eligendi neque sint nobis perferendis, blanditiis maxime eaque architecto molestiae ratione odio fugiat obcaecati id. Soluta illum, quaerat, possimus repudiandae deserunt debitis maiores eligendi ad quae, dolorum dicta dolores. Labore, sit! Fugit similique, aliquid necessitatibus doloremque rerum a, adipisci ut quos facere voluptatibus iure saepe, beatae nam id delectus facilis laboriosam blanditiis nostrum inventore exercitationem est. Consequuntur consectetur nesciunt rem iure. Optio vero iste eos aut voluptate temporibus. Placeat reprehenderit perferendis a nisi id, cum consequatur ad expedita, rem assumenda, odit officiis nostrum nesciunt sed inventore!
+                    </div>
+
+                    <button className="learn-more">Learn More</button>
+
+                </div>
+
+            </div>
         </>
     )
 }
